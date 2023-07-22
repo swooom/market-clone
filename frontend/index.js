671e2c13c8d91b06ad0889f5af34591869536429
swooom <137802892+swooom@users.noreply.github.com>
2023-07-22 20:32:18 +0900

    [당근마켓클론]
    -글쓰기페이지 form(하위페이지) 작성
    --백엔드와 SQLite를 연동하여 이미지 및 데이터 업로드 구현
    
    -메인 페이지에 DB연동하여 불러오기

---
+++ frontend/index.js
@@ -0,0 +1,63 @@
+function calcTime(timestamp) {
+  //한국시간 UTC+9
+  const curTime = new Date().getTime() - 9 * 60 * 60 * 1000;
+  const time = new Date(curTime - timestamp);
+  const hour = time.getHours();
+  const minute = time.getMinutes();
+  const second = time.getSeconds();
+
+  if (hour > 0) return `${hour}시간전`;
+  else if (minute > 0) return `${minute}분 전`;
+  else if (second > 0) return `${second}초 전`;
+  else return "방금 전";
+}
+
+function renderData(data) {
+  const main = document.querySelector("main");
+
+  data.reverse().forEach(async (obj) => {
+    const div = document.createElement("div");
+    div.className = "items-list";
+
+    const imgDiv = document.createElement("div");
+    imgDiv.className = "item-list__img";
+
+    const img = document.createElement("img");
+    const res = await fetch(`/images/${obj.id}`);
+    const blob = await res.blob();
+    const url = URL.createObjectURL(blob);
+    img.src = url;
+
+    const InfoDiv = document.createElement("div");
+    InfoDiv.className = "item-list__info";
+
+    const InfoTitleDiv = document.createElement("div");
+    InfoTitleDiv.className = "item-list__info-title";
+    InfoTitleDiv.innerText = obj.title;
+
+    const InfoMetaDiv = document.createElement("div");
+    InfoMetaDiv.className = "item-list__info-meta";
+    InfoMetaDiv.innerText = obj.place + "\t" + calcTime(obj.insertAt);
+
+    const InfoPriceDiv = document.createElement("div");
+    InfoPriceDiv.className = "item-list__info-price";
+    InfoPriceDiv.innerText = obj.price;
+
+    InfoDiv.appendChild(InfoTitleDiv);
+    InfoDiv.appendChild(InfoMetaDiv);
+    InfoDiv.appendChild(InfoPriceDiv);
+    imgDiv.appendChild(img);
+
+    div.appendChild(imgDiv);
+    div.appendChild(InfoDiv);
+    main.appendChild(div);
+  });
+}
+
+async function fetchList() {
+  const res = await fetch("/items");
+  const data = await res.json();
+  renderData(data);
+}
+
+fetchList();
